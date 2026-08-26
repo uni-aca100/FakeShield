b239184db121bc22bed677d0e4ffedd221ac5bd9
uni-aca100 <davide.duraccio001@studenti.uniparthenope.it>
2026-08-26 18:34:53 +0200

    add cache_position=none

--- DTE_FDM/llava/model/language_model/llava_llama.py
+++ DTE_FDM/llava/model/language_model/llava_llama.py
@@ -68,6 +68,8 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         images: Optional[torch.FloatTensor] = None,
         image_sizes: Optional[List[List[int]]] = None,
         return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        **kwargs
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
         if inputs_embeds is None:
