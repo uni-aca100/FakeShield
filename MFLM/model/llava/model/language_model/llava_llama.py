4fc86fb3d2b38132290c24c9cfabf4d6a324e098
uni-aca100 <davide.duraccio001@studenti.uniparthenope.it>
2026-08-26 18:20:50 +0200

    a

--- MFLM/model/llava/model/language_model/llava_llama.py
+++ MFLM/model/llava/model/language_model/llava_llama.py
@@ -103,7 +103,6 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
-            cache_position=cache_position,
             **kwargs
         )
 
